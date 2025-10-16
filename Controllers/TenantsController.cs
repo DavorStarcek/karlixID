99c05fa48d8001212d286f00ae09f03b0b531b3b
Davor Starcek <Davor.Starcek@mstartplus.hr>
2025-10-16 17:15:31 +0200

    ds

---
+++ Controllers/TenantsController.cs
@@ -0,0 +1,30 @@
+﻿using KarlixID.Web.Data;
+using KarlixID.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KarlixID.Web.Controllers
+{
+    [Authorize(Roles = AppRoleInfo.GlobalAdmin)]
+    public class TenantsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TenantsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /Tenants
+        public async Task<IActionResult> Index()
+        {
+            var data = await _db.Tenants
+                .AsNoTracking()
+                .OrderBy(t => t.Name)
+                .ToListAsync();
+
+            return View(data);
+        }
+    }
+}
