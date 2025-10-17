bda25753522e2b6aac9aa392b3e6d30aa746d766
Davor Starcek <Davor.Starcek@mstartplus.hr>
2025-10-17 12:25:37 +0200

    radi super

--- obj/Debug/net8.0/KarlixID.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/KarlixID.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarlixID.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e80b4d4d522e842140eecef9d8816b03ed2cf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429c1713684dd6d46f36d5f22a5fbe151d844578")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarlixID.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarlixID.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
